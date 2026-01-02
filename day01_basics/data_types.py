9df4c0aed85a9d8d9e106e1a3a75ad9c2ca77c41
drawts10 <stward10jb@hotmail.com>
2026-01-01 21:16:34 -0800

    Day 01: variables, data types, and basic o perations

---
+++ day01_basics/data_types.py
@@ -0,0 +1,7 @@
+# Data Types
+
+int = 26 #Number intenger
+float = 26.5 # Number decimal
+str = "Mike" # Names
+bool = True / False # Classification 
+
---
+++ day01_basics/variables.py
@@ -0,0 +1,8 @@
+# Variables
+
+name = "Mike" #String
+age = 25 #Intenger
+is_student = True #Booleang
+
+x = 25 # A variable shound't have one letter
+user_age = 25 # Should display the reason for the variable
